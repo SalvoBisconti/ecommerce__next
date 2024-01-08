f72530a413ee412654d3595d8fd0885deedef4e5
SalvoBisconti <salvatorebisconti98@gmail.com>
2024-01-08 23:11:31 +0100

    update

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -18,7 +18,7 @@ export const Card = (props: { data: cardType }) => {
 
   return (
     <div
-      className="h-[450px] w-[320px] min-w-[310px] relative flex flex-col justify-center shadow-md rounded-md cursor-pointer  "
+      className="h-[450px] w-[320px] min-w-[310px] relative flex flex-col justify-center shadow-md rounded-md cursor-pointer   "
       onClick={onHandleOpenSingleItem}
     >
       <div className="h-[50%] relative ">
@@ -35,21 +35,26 @@ export const Card = (props: { data: cardType }) => {
           <p className="text-[13px]">Novità</p>
         </div>
       </div>
-      <div className=" h-[50%] flex flex-col justify-center items-center">
-        <h3 className="font-bold"> {data.title}</h3>
-        <h3 className="p-4">
-          {data.description.split(" ").slice(0, 5).join(" ")} {". . ."}
-        </h3>
-        <h3 className="text-red text-[20px] mb-1 ">
-          {toPercentuagePrice(data.price, 15).toFixed(2)} {"€"}
-        </h3>
-        <div className="flex gap-2 items-center ">
-          <h3 className="line-through text-[18px]">
-            {data.price.toFixed(2)} {"€"}
+      <div className=" h-[50%] flex flex-col justify-center items-center gap-7 p-4">
+        <div className="flex flex-col items-center gap-[2px]">
+          <h3 className="font-light">{data.category.name}</h3>
+          <h3 className="font-bold"> {data.title}</h3>
+          <h3 className="text-[#666666]">
+            {data.category.id} {data.category.id == 1 ? "colore" : "colori"}
           </h3>
-          <h3 className="bg-green text-first px-2 py-[1px] rounded text-[15px] font-semibold">
-            -15%
+        </div>
+        <div className="flex flex-col items-center">
+          <h3 className="text-red text-[20px] mb-1 ">
+            {toPercentuagePrice(data.price, 15).toFixed(2)} {"€"}
           </h3>
+          <div className="flex gap-2 items-center ">
+            <h3 className="line-through text-[18px]">
+              {data.price.toFixed(2)} {"€"}
+            </h3>
+            <h3 className="bg-green text-first px-2 py-[1px] rounded text-[15px] font-semibold">
+              -15%
+            </h3>
+          </div>
         </div>
       </div>
     </div>
